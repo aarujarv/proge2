e55df562998ed60f6b787a1f26a842f772e33899
aarujarv <a.arujarv@gmail.com>
2023-01-08 18:58:18 +0200

    new

---
+++ chucky/Program.cs
@@ -0,0 +1,15 @@
+﻿string path = @"C:\Users\aaruj\Google Drive\!KOOL\!Kool\Programmeerimise alused\data";
+string fileName = "chucky.txt";
+string[] lines = File.ReadAllLines(Path.Combine(path,fileName));
+
+ShowFileContent(lines);
+
+static Array ShowFileContent(string[] fileContentArray)
+{
+    int i = ;
+    foreach (string line in fileContentArray)
+    {
+        Console.WriteLine($"{i}. {...}");
+        i++;
+    }
+}
